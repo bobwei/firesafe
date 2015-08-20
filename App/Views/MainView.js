080120b4347794081a5ccff6a435229469f4ca63
bobwei <bob61016@gmail.com>
2015-08-20 15:27:04 +0800

    add NavigatorIOS

---
+++ App/Views/MainView.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MainView = React.createClass({
+  render: function() {
+    return (
+      <View>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+});
+
+module.exports = MainView;
--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
+  NavigatorIOS,
+  StatusBarIOS,
   StyleSheet,
   Text,
   View,
 } = React;
+var MainView = require('./App/Views/MainView');
 
 var firesafe = React.createClass({
   render: function() {
+    StatusBarIOS.setStyle('light-content');
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          component: MainView,
+          title: 'Firesafe'
+        }}
+        tintColor="#FFFFFF"
+        barTintColor="#24CA99"
+        titleTextColor="#FFFFFF"
+        translucent="true"
+      />
     );
   }
 });
@@ -34,20 +34,7 @@ var firesafe = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('firesafe', () => firesafe);
